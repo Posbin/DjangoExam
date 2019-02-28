f2863dd2ef903d26bbc452bdab880d836da2415a
masashi <masashi@masashinoMacBook-puro.local>
2019-03-01 00:45:44 +0900

    販売統計情報 集計処理修正

--- FruitSalesMgmt/views.py
+++ FruitSalesMgmt/views.py
@@ -6,7 +6,7 @@ from .models import Fruit, Sale
 from .forms import FruitForm, SaleForm
 
 def top(request):
-    return render(request, 'FruitSalesMgmt/top.html', {})
+    return render(request, 'FruitSalesMgmt/top.html')
 
 def fruits_list(request):
     fruits = Fruit.objects.all()
@@ -77,65 +77,58 @@ def stats(request):
     all_stats = Stat(all_sales)
     monthly_stats = get_monthly_stats(5)
     daily_stats = get_daily_stats(5)
-    return render(request, 'FruitSalesMgmt/stats.html', {'all_stats_total': all_stats.total, 'monthly_stats': monthly_stats, 'daily_stats': daily_stats})
+
+    context = {
+        'all_stats_total': all_stats.total,
+        'monthly_stats': monthly_stats,
+        'daily_stats': daily_stats
+    }
+    return render(request, 'FruitSalesMgmt/stats.html', context)
 
 def get_monthly_stats(num):
+    stats = {}
     ago = num - 1
     today = timezone.now()
-    begin_date = (today - relativedelta(months=ago)).replace(day=1)        
-    stats = get_init_monthly_stats(begin_date, today)
-
-    sales = Sale.objects.filter(datetime__gte=begin_date)
-    for sale in sales:
-        key = sale.datetime.strftime('%y/%m')
-        stats[key].append_sale_data(sale)
-    return stats
-
-def get_daily_stats(num):
-    ago = num - 1
-    today = timezone.now()
-    begin_date = today - timedelta(days=ago)
-    stats = get_init_daily_stats(begin_date, today)
-
-    sales = Sale.objects.filter(datetime__gte=begin_date)
-    for sale in sales:
-        key = sale.datetime.strftime('%y/%m/%d')
-        stats[key].append_sale_data(sale)
-    return stats
-
-def get_init_daily_stats(from_date, to_date):
-    stats = {}
-    date_i = from_date
-    while date_i <= to_date:
-        key = '{0}/{1}/{2}'.format(date_i.year, date_i.month, date_i.day)
-        stats[key] = Stat()
-        date_i += timedelta(days=1)
-    return stats
-
-def get_init_monthly_stats(from_date, to_date):
-    stats = {}
-    date_i = from_date
-    while date_i <= to_date:
-        key = '{0}/{1}'.format(date_i.year, date_i.month)
-        stats[key] = Stat()
+    date_i = (today - relativedelta(months=ago)).replace(day=1)        
+    while date_i <= today:
+        year = date_i.year
+        month = date_i.month
+        key = "{0}/{1}".format(year, month)
+        sales = Sale.objects.filter(datetime__year=year,
+                                    datetime__month=month)
+        stats[key] = Stat(sales)
         date_i += relativedelta(months=1)
     return stats
 
-class Stat:
-    def __init__(self, sales=[]):
-        self.sales = sales
-
-    def append_sale_data(self, sale):
-        self.sales.append(sale)
+def get_daily_stats(num):
+    stats = {}
+    ago = num - 1
+    today = timezone.now()
+    date_i = today - timedelta(days=ago)
+    while date_i <= today:
+        year = date_i.year
+        month = date_i.month
+        day = date_i.day
+        key = "{0}/{1}/{2}".format(year, month, day)
+        sales = Sale.objects.filter(datetime__year=year,
+                                    datetime__month=month,
+                                    datetime__day=day)
+        stats[key] = Stat(sales)
+        date_i += timedelta(days=1)
+    return stats
     
-    @property
-    def details(self):
-        details_list = [detail(sale) for sale in self.sales]
-        return ', '.join(details_list)
-
-    def detail(sale):
-        return '{0}: {1}円({2})'.format(sale.name, sale.total, sale.number)
+class Stat:
+    def __init__(self, sales):
+        self.sales = sales
 
     @property
     def total(self):
         return sum([sale.total for sale in self.sales])
+
+    @property
+    def details(self):
+        details_list = [self.__detail(sale) for sale in self.sales]
+        return ', '.join(details_list)
+
+    def __detail(self, sale):
+        return '{0}: {1}円({2})'.format(sale.fruit.name, sale.total, sale.number)
